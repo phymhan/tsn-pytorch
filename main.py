c5749107ea2fb77eeeb9eeaea0740838d8d91b15
phymhan <hanligong@gmail.com>
2019-07-29 18:53:39 -0400

    lazy

--- main.py
+++ main.py
@@ -145,7 +145,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
     data_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
-    top5 = AverageMeter()
+    # top5 = AverageMeter()
 
     if args.no_partialbn:
         model.module.partialBN(False)
@@ -169,10 +169,11 @@ def train(train_loader, model, criterion, optimizer, epoch):
         loss = criterion(output, target_var)
 
         # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.data, target, topk=(1,5))
+        # prec1, prec5 = accuracy(output.data, target, topk=(1,5))
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 1))
         losses.update(loss.data[0], input.size(0))
         top1.update(prec1[0], input.size(0))
-        top5.update(prec5[0], input.size(0))
+        # top5.update(prec5[0], input.size(0))
 
 
         # compute gradient and do SGD step
@@ -192,21 +193,28 @@ def train(train_loader, model, criterion, optimizer, epoch):
         end = time.time()
 
         if i % args.print_freq == 0:
+            # print(('Epoch: [{0}][{1}/{2}], lr: {lr:.5f}\t'
+            #       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+            #       'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+            #       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+            #       'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+            #       'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+            #        epoch, i, len(train_loader), batch_time=batch_time,
+            #        data_time=data_time, loss=losses, top1=top1, top5=top5, lr=optimizer.param_groups[-1]['lr'])))
             print(('Epoch: [{0}][{1}/{2}], lr: {lr:.5f}\t'
-                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                   epoch, i, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses, top1=top1, top5=top5, lr=optimizer.param_groups[-1]['lr'])))
+                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                   'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                epoch, i, len(train_loader), batch_time=batch_time,
+                data_time=data_time, loss=losses, top1=top1, lr=optimizer.param_groups[-1]['lr'])))
 
 
 def validate(val_loader, model, criterion, iter, logger=None):
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
-    top5 = AverageMeter()
+    # top5 = AverageMeter()
 
     # switch to evaluate mode
     model.eval()
